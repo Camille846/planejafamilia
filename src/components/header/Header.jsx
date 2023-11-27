f5dfe835d598650c2c3aa006c58e522633bda2cc
Camille Gomes <83260908+Camille846@users.noreply.github.com>
2023-11-27 20:49:16 -0300

    Feat: header component and styles

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,69 @@
+import HeaderSvg from '../../assets/images/HeaderSvg'
+import Navbar from '../navbar/Navbar'
+import {
+  headerWrapper,
+  headerContainer,
+  headerContent,
+  headerTitle,
+  headerDescription,
+  headerButtons,
+  headerButton,
+  headerImages,
+  headerImageBorder,
+  headerImage
+} from './styles.module.css'
+
+
+const Header = () => {
+  return (
+    <>
+        <Navbar />
+        <section className={headerWrapper}>
+          <div className={headerContainer}>
+            <div className={headerImages}>
+              
+              <div className={headerImageBorder}>	
+                <div className={headerImage}>
+                  <img src=
+                  "../headerImage.jpg"
+                    alt="header Image"
+                  />
+                </div>
+              </div>
+              <div className={headerImageBorder}>	
+                <div className={headerImage}>
+                  <img src=
+                  "../headerImage2.jpg"
+                    alt="header Image"
+                  />
+                </div>
+              </div>
+              <div className={headerImageBorder}>	
+                <div className={headerImage}>
+                  <img src=
+                  "../headerImage3.jpg"
+                    alt="header Image"
+                  />
+                </div>
+              </div>
+            </div>
+            <div className={headerContent}>
+              <h1 className={headerTitle}>
+                Planejamento 
+                <span>reprodutivo</span>
+              </h1>
+              <p className={headerDescription}>
+                Planejamento reprodutivo é o direito que toda pessoa tem à informação, à assistência especializada e ao acesso aos recursos que permitam optar livre e  conscientemente por ter ou não ter filhos.
+              </p>
+              <div className={headerButtons}>
+                <button className={headerButton}>Comece agora</button>
+                <button className={headerButton}>Saiba mais</button>
+              </div>
+            </div>
+          </div>
+        </section>
+    </>
+  )
+}
+
+export default Header
